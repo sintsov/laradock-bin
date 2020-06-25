8492511f879ba0ff75830c84a8a6ad9f653a64f2
Синцов Роман <sintsov.rv@gazprom-neft.ru>
2020-06-25 23:13:34 +0300

    Fix problem with path for symbol link

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+pwd=`pwd -P`
 chmod +x php composer npm phpunit yarn mysql mysqldump
-ln -s php /bin/php
-ln -s composer /bin/composer
-ln -s npm /bin/npm
-ln -s phpunit /bin/phpunit
-ln -s yarn /bin/yarn
-ln -s mysql /bin/mysql
-ln -s mysqldump /bin/mysqldump
+ln -s ${pwd}/php /bin/php
+ln -s ${pwd}composer /bin/composer
+ln -s ${pwd}npm /bin/npm
+ln -s ${pwd}phpunit /bin/phpunit
+ln -s ${pwd}yarn /bin/yarn
+ln -s ${pwd}mysql /bin/mysql
+ln -s ${pwd}mysqldump /bin/mysqldump
