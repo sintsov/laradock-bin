4cef8596d281613329b02f89c6c2a42b0a9f5552
Синцов Роман <sintsov.rv@gazprom-neft.ru>
2020-06-25 23:15:08 +0300

    Fix path

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@
 pwd=`pwd -P`
 chmod +x php composer npm phpunit yarn mysql mysqldump
 ln -s ${pwd}/php /bin/php
-ln -s ${pwd}composer /bin/composer
-ln -s ${pwd}npm /bin/npm
-ln -s ${pwd}phpunit /bin/phpunit
-ln -s ${pwd}yarn /bin/yarn
-ln -s ${pwd}mysql /bin/mysql
-ln -s ${pwd}mysqldump /bin/mysqldump
+ln -s ${pwd}/composer /bin/composer
+ln -s ${pwd}/npm /bin/npm
+ln -s ${pwd}/phpunit /bin/phpunit
+ln -s ${pwd}/yarn /bin/yarn
+ln -s ${pwd}/mysql /bin/mysql
+ln -s ${pwd}/mysqldump /bin/mysqldump
