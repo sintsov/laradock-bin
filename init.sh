e4d6fb2f06cac0a38ee102d66502d7ca12b6725f
Синцов Роман <sintsov.rv@gazprom-neft.ru>
2020-06-25 23:08:48 +0300

    Mysql & Mysqldump

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+chmod +x php composer npm phpunit yarn mysql mysqldump
+ln -s php /bin/php
+ln -s composer /bin/composer
+ln -s npm /bin/npm
+ln -s phpunit /bin/phpunit
+ln -s yarn /bin/yarn
+ln -s mysql /bin/mysql
+ln -s mysqldump /bin/mysqldump
